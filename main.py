3cb240164ddeddbb70c051928f9f9d7e51d6f64c
External Host <haydenr261@gmail.com>
2025-05-31 12:59:40 -0700

    pipe impl

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 # third party libs
+from random import randint
 import pygame
 import pyaudio
 
 # our libs
 from player import Player
+from pipe import Pipe
 
+
+SCREEN_DIMENSIONS = (1280, 720)
+SCREEN = pygame.display.set_mode((SCREEN_DIMENSIONS[0], SCREEN_DIMENSIONS[1]))
 
 CHUNK = 1024 # samples per frame
 FORMAT = pyaudio.paInt16  # audio format (16-bit PCM)
@@ -12,12 +17,40 @@ CHANNELS = 1 # one channel
 RATE = 44100 # samples per second
 
 DARK_GREEN = (38, 118, 32)
+PIPE_NUM = 10
+PIPE_HORIZ_DISTANCE = 150
+PIPE_VERT_DISTANCE = 50
+GRAVITY_CONSTANT = 6
+
+pipes = []
+
+
+def generatePipes():
+    pipes.clear()
+    for i in range(PIPE_NUM):
+        x = i * PIPE_HORIZ_DISTANCE
+        center_y = randint(150, SCREEN_DIMENSIONS[1] - 150)
+
+        top_height = center_y - (PIPE_VERT_DISTANCE // 2)
+        bottom_y = center_y + (PIPE_VERT_DISTANCE // 2)
+        bottom_height = SCREEN_DIMENSIONS[1] - bottom_y
+
+        r = randint(0, 50) 
+        g = randint(150, 255)
+        b = randint(0, 50)
+        col = (r, g, b)
+
+        pipes.append(Pipe(col, top_height, x))
+        pipes.append(Pipe(col, bottom_height, x, bottom_y)) 
+
+def drawAllPipes():
+    for pipe in pipes:
+        pipe.draw(SCREEN)
 
 class Game:
     def __init__(self) -> None:
-        self.screen = pygame.display.set_mode((1280,720))
         self.stopLoop = False
-        self.rectangle = pygame.Rect(30,60,90,60)
+        generatePipes()
 
         self.player = Player()
         p = pyaudio.PyAudio()
@@ -30,29 +63,19 @@ class Game:
     def onStart(self):
         pygame.init()
         
-    
     def onLoop(self):
-        pygame.draw.rect(self.screen, DARK_GREEN, (150,0,40,250))
-        #self.rectangle.move(90,120)
-        pygame.draw.rect(self.screen, DARK_GREEN, (150,450,40,400))
-        pygame.draw.rect(self.screen, DARK_GREEN, (350,0,40,200))
-        pygame.draw.rect(self.screen, DARK_GREEN, (350,400,40,350))
-        pygame.draw.rect(self.screen, DARK_GREEN, (550,0,40,150))
-        pygame.draw.rect(self.screen, DARK_GREEN, (550,350,40,400))
-        pygame.draw.rect(self.screen, DARK_GREEN, (750,0,40,150))
-        pygame.draw.rect(self.screen, DARK_GREEN, (750,450,40,400))
-        pygame.draw.rect(self.screen, DARK_GREEN, (950,450,40,400))
-        pygame.display.flip()
-        self.player.draw(self.screen)
+        drawAllPipes()
+        self.player.draw(SCREEN)
+        self.player.move(0, -GRAVITY_CONSTANT)
 
-        self.player.move(0, -5)
         processEvents()
         processMicrophone()
+
+        pygame.display.flip()
         return
-    
     
     def onEnd(self):
-        return
+        pygame.quit()
 
 def processEvents():
     for event in pygame.event.get():
