47ae09edab53e03a03c0c3203b0b1867835094d8
External Host <haydenr261@gmail.com>
2025-05-31 12:24:08 -0700

    added move

--- main.py
+++ main.py
@@ -43,6 +43,9 @@ class Game:
         pygame.draw.rect(self.screen, DARK_GREEN, (750,450,40,400))
         pygame.draw.rect(self.screen, DARK_GREEN, (950,450,40,400))
         pygame.display.flip()
+        self.player.draw(self.screen)
+
+        self.player.move(0, -5)
         processEvents()
         processMicrophone()
         return
